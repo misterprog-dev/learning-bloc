d46f8fb9f3fdd165f14f332b3986f471ee81af02
dsoumaila <soumdiakite182@gmail.com>
2024-04-26 04:12:27 +0000

    Training on Cubit and Bloc

--- lib/training/main2.dart
+++ lib/training/main2.dart
@@ -1,8 +1,3 @@
-// https://dartpad.dev/?id=fdd369962f4ff6700a83c8a540fd6c4c
-// This code is distributed under the MIT License.
-// Copyright (c) 2018 Felix Angelov.
-// You can find the original at https://github.com/felangel/bloc.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
